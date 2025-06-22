a67e1d4a21001952fa55937a0296c1528336a791
xxMihaix <marius777bogdan@gmail.com>
2025-06-22 18:04:06 +0300

    Add files via upload

---
+++ send.js
@@ -0,0 +1,10 @@
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const btn = document.getElementById('go');
+
+    btn.addEventListener('click', function(){
+        window.location.href = "index.html#all-contact-container";
+    })
+})
